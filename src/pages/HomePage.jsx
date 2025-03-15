184a6ecd5b63d3aa5cca56f643c7808362c53de2
smath <smathewslynch@gmail.com>
2025-03-15 21:53:12 +0000

    Slight edit to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Layout from '../components/Layout';
+
+
 /**References:
  * https://www.youtube.com/watch?v=znbCa4Rr054 **/
 
